a56750dc0fbe9c55d370359b2999b878e3a791c0
H-Blues <2972095695@qq.com>
2022-09-17 21:44:25 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
-  owner: "Joe Bloggs",
+  owner: 'Joe Bloggs',
+  address: '3 Walkers Lane',
+  previous_owners: [
+    { name: 'Pat Smith', address: '1 Main Street' },
+    { name: 'Sheila Dwyer', address: '2 High Street' }
+  ],
   type: {
     make: 'Toyota',
     model: 'Corolla',
@@ -12,7 +17,7 @@ const aCar = {
   }
 }
 
-console.log(aCar.owner + 'drives a ' + aCar.type.make)
+console.log(aCar.owner + "drives a " + aCar.type.make)
 console.log("Reg. = " + aCar.registration.year + "-" +
   aCar.registration.county + "-" + aCar.registration.number)
 
