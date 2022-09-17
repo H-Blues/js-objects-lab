c3e4526398445a61eca16caa9beb4e5f7bfd2354
H-Blues <2972095695@qq.com>
2022-09-17 21:36:56 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -13,4 +13,17 @@ const aCar = {
 }
 
 console.log(aCar.owner + 'drives a ' + aCar.type.make)
-console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.county + "-" + aCar.registration.number)
+console.log("Reg. = " + aCar.registration.year + "-" +
+  aCar.registration.county + "-" + aCar.registration.number)
+
+aCar.mileage = 10000
+aCar.color = {
+  exterior: 'red',
+  interior: {
+    texture: 'leather',
+    shade: 'cream',
+  }
+}
+
+console.log("It is a " + aCar.color.exterior + " car, " +
+  aCar.mileage + " mileage, with " + aCar.color.interior.texture + " interior.")
